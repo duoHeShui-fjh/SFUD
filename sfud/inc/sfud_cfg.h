94908a6d61c19685f8b06d47478e7cbcddf2fd62
duoHeShui-fjh <1512140371@qq.com>
2025-09-25 12:18:27 +0800

    Update device index and flash device table in sfud_cfg.h

--- sfud/inc/sfud_cfg.h
+++ sfud/inc/sfud_cfg.h
@@ -38,12 +38,12 @@
 #define SFUD_USING_FLASH_INFO_TABLE
 
 enum {
-    SFUD_XXXX_DEVICE_INDEX = 0,
+    SFUD_DEVICE_INDEX_0 = 0,
 };
 
 #define SFUD_FLASH_DEVICE_TABLE                                                \
 {                                                                              \
-    [SFUD_XXXX_DEVICE_INDEX] = {.name = "XXXX", .spi.name = "SPIX"},           \
+    [SFUD_XXXX_DEVICE_INDEX] = {.name = "W25Q128", .spi.name = "SPI1"},           \
 }
 
 // #define SFUD_USING_QSPI
